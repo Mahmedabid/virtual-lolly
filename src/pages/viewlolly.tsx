399d7813217aaadce1c41c92d279fc80ff49a018
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-05-20 11:34:21 +0500

    Update viewlolly.tsx

--- src/pages/viewlolly.tsx
+++ src/pages/viewlolly.tsx
@@ -18,14 +18,14 @@ const GetLolly_by_path = gql`
 
 export default (props: any) => {
 
-    var refresh = window.localStorage.getItem('refresh');
-    if (refresh === null) {
-        window.location.reload();
-        window.localStorage.setItem('refresh', "1");
+    if (window) {
+        var refresh = window.localStorage.getItem('refresh');
+        if (refresh === null) {
+            window.location.reload();
+            window.localStorage.setItem('refresh', "1");
+        }
     }
-
     const path = props.location.pathname.replace("/viewlolly/", "")
-
 
     const { loading, error, data } = useQuery(GetLolly_by_path, {
         variables: {
