54578bdd24073d94340a1fdcca10c911311560c5
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-05-22 10:35:54 +0500

    ok

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,15 +6,16 @@ interface Button {
     type?: "button" | "reset" | "submit"
     onClick?: () => void
     style?: {}
-    className: string
+    className?: string
 }
 
 const Button: React.FC<Button> = ({value, type, onClick, style, className}) => {
 
     const typeVal = type? type: "button";
+    const classNameVal = className? className: '';
 
     return (
-        <button className={`SBbutton ${className}`} type={typeVal} onClick={onClick} style={{...style}}>
+        <button className={`SBbutton ${classNameVal}`} type={typeVal} onClick={onClick} style={{...style}}>
             {value}
         </button>
     )
